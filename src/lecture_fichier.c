8a3f25ff93f28255ffb66079db32cf0b0e15729b
phelma <phelma@localhost.localdomain>
2021-09-26 07:32:01 -0400

    Yassine: correction erreur dans lecture_fichier.c

--- src/lecture_fichier.c
+++ src/lecture_fichier.c
@@ -3,6 +3,7 @@
 queue_t lecture_fichier_conf(queue_t lexemes_q, char * config)
 {
   char buffer[256];
+  char comment_test[256];
 
   // 1 - Ouverture du fichier de configuration
   FILE* fichier = NULL;
@@ -14,18 +15,19 @@ queue_t lecture_fichier_conf(queue_t lexemes_q, char * config)
   }
 
 // 2 - Lecture du fichier de configuration et complétion des champs décrivant les lexemes (le nom, l'expression régulière en caractères, l'expression régulière en queue)
-  while(!feof(fichier))
+  while(!feof(fichier)) // Tant que ce n'est pas la fin du fichier source...
   {
-    struct lexeme * lexeme;
-    lexeme=calloc(1,sizeof(*lexeme));
-    fscanf(fichier,"%s",lexeme->name);
-    if((lexeme->name)[0]!='#')
+    fscanf(fichier,"%s",comment_test);
+    if(comment_test[0]!='#') // Si la ligne n'est pas un commentaire...
     {
-      fscanf(fichier,"%s",lexeme->regexp_str);
-      lexeme->queue_regexp=re_read(lexeme->regexp_str);
-      lexemes_q=enqueue(lexemes_q,lexeme);
+      struct lexeme * lexeme; // On crée un type lexeme
+      lexeme=calloc(1,sizeof(*lexeme)); // On lui alloue de la mémoire
+      lexeme->regexp_name=comment_test;// Et on stock son nom
+      fscanf(fichier,"%s",lexeme->regexp_str); // ainsi que son expression régulière
+      lexeme->queue_regexp=re_read(lexeme->regexp_str); // On "transforme" l'expression reg. (qui est en type char*) en type char group.
+      lexemes_q=enqueue(lexemes_q,lexeme); // On insère ce lexème dans la file.
     }
-    else
+    else // Sinon on stock cette ligne dans une variable tampon qu'on n'utilisera pas.
     {
       fgets(buffer,256,fichier);
     }
