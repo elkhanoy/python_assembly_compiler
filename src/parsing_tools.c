bf347725a59ae373dd34209af58c4e7e3eadf434
phelma <phelma@localhost.localdomain>
2021-10-03 13:04:10 -0400

    Fin des fonctions de base pour le parsing, Siryne

--- src/parsing_tools.c
+++ src/parsing_tools.c
@@ -16,11 +16,17 @@ lexem_t lexem_advance(list_t *lexems)
 // Déterminer si le prochain lexème utile est bien du type demandé
 int next_lexem_is(list_t *lexems, char *type)
 {
-  
+  if(((*lexems)->content)->type == (*type))
+  {
+    return 0;
+  }
+  else
+    {return -1;}
 }
 ////////////////////////////////////////////////////////////////////////////////
 // Affichager le message d'erreur msg et les coordonnées du lexème en tête de *lexems
 void print_parse_error(char *msg, list_t *lexems)
 {
-
+  printf("%s \n",(*msg));
+  printf("Les coordonnées du lexème en tête de liste sont: ligne: %d et colonne: %d \n", ((*lexems)->content)->line,((*lexems)->content)->column);
 }
