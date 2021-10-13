fd804de9edc4a03f8a366c2f2d6e4796e59bfec9
phelma <phelma@localhost.localdomain>
2021-10-13 09:30:11 -0400

    Siryne - corrections prototypes parsing_nonterm

--- src/parsing_nonterminal.c
+++ src/parsing_nonterminal.c
@@ -1,7 +1,7 @@
 #include <pyas/all.h>
 
 //〈pys〉: :=〈eol〉〈prologue〉〈code〉
-int parse_pys(list_t * lexems)
+int parse_pys(list_t * lexems,pyobj_t obj)
 {
   printf("Parsing pys expression \n");
   if(-1==parse_eol(lexems))
@@ -22,7 +22,7 @@ int parse_pys(list_t * lexems)
 }
 ////////////////////////////////////////////////////////////////////////////////
 //〈prologue〉: :=〈set-directives〉 〈interned-strings〉 〈constants〉 〈names〉〈varnames〉〈freevars〉〈cellvars〉
-int parse_prologue(list_t * lexems)
+int parse_prologue(list_t * lexems,pyobj_t obj)
 {
   printf("Parsing prologue expression \n");
   if(-1==parse_set_directives(lexems))
@@ -71,7 +71,7 @@ int parse_prologue(list_t * lexems)
 }
 ////////////////////////////////////////////////////////////////////////////////
 //〈set-directives〉: :=〈set-version-pyvm〉 〈set-flags〉 〈set-filename〉 〈set-name〉[〈set-source-size〉]〈set-stack-size〉 〈set-arg-count〉[set-kwonly-arg-count][set-posonly-arg-count]
-int parse_set_directives(list_t * lexems)
+int parse_set_directives(list_t * lexems,pyobj_t obj)
 {
   if(-1==parse_set_version_pyvm(lexems))
   {
@@ -113,7 +113,7 @@ int parse_set_directives(list_t * lexems)
   return 0;
 }
 //〈set-version-pyvm〉: :=  {‘dir::set’}{‘blank’}{‘version_pyvm’}{‘blank’}{‘integer::dec’}〈eol〉
-int parse_set_version_pyvm(list_t * lexems)
+int parse_set_version_pyvm(list_t * lexems,pyobj_t obj)
 {
   printf("Parsing set-version-pyvm \n");
   if(0==next_lexem_is(lexems, "dir::set"))
@@ -163,7 +163,7 @@ int parse_set_version_pyvm(list_t * lexems)
   return 0;
 }
 //〈set-flags〉: :=  {‘dir::set’} {‘blank’} {‘flags’} {‘blank’} {‘integer::hex’}〈eol〉
-int parse_set_flags(list_t * lexems)
+int parse_set_flags(list_t * lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -212,7 +212,7 @@ int parse_set_flags(list_t * lexems)
   return 0;
 }
 //〈set-filename〉: :=  {‘dir::set’} {‘blank’} {‘filename’} {‘blank’} {‘string’}〈eol〉
-int parse_set_filename(list_t * lexems)
+int parse_set_filename(list_t * lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -258,10 +258,10 @@ int parse_set_filename(list_t * lexems)
   {
     return -1;
   }
-  return 0;
+return 0;
 }
 //〈set-name〉: :=  {‘dir::set’} {‘blank’} {‘name’} {‘blank’} {‘string’}〈eol〉
-int parse_set_name(list_t * lexems)
+int parse_set_name(list_t * lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -310,7 +310,7 @@ int parse_set_name(list_t * lexems)
   return 0;
 }
 //〈set-source-size〉: :=  {‘dir::set’}{‘blank’}{‘source_size’}{‘blank’}{‘integer::dec’}〈eol〉
-int parse_set_source_size(list_t * lexems)
+int parse_set_source_size(list_t * lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -359,7 +359,7 @@ int parse_set_source_size(list_t * lexems)
   return 0;
 }
 //〈set-stack-size〉: :=  {‘dir::set’}{‘blank’}{‘stack_size’}{‘blank’}{‘integer::dec’}〈eol〉
-int parse_set_stack_size(list_t * lexems)
+int parse_set_stack_size(list_t * lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -408,7 +408,7 @@ int parse_set_stack_size(list_t * lexems)
   return 0;
 }
 //〈set-arg-count〉: :=  {‘dir::set’}{‘blank’}{‘arg_count’}{‘blank’}{‘integer::dec’}〈eol〉
-int parse_set_arg_count(list_t*lexems)
+int parse_set_arg_count(list_t*lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -457,7 +457,7 @@ int parse_set_arg_count(list_t*lexems)
   return 0;
 }
 //〈set-kwonly-arg-count〉: :=  {‘dir::set’}{‘blank’}{‘kwonly_arg_count’}{‘blank’} {‘integer::dec’}〈eol〉
-int parse_set_kwonly_arg_count(list_t*lexems)
+int parse_set_kwonly_arg_count(list_t*lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -506,7 +506,7 @@ int parse_set_kwonly_arg_count(list_t*lexems)
   return 0;
 }
 //〈set-posonly-arg-count〉: :=  {‘dir::set’}{‘blank’}{‘posonly_arg_count’}{‘blank’} {‘integer::dec’}〈eol〉
-int parse_set_posonly_arg_count(list_t*lexems)
+int parse_set_posonly_arg_count(list_t*lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::set"))
   {
@@ -580,7 +580,7 @@ int parse_set(list_t * lexems, pyobj_t obj) {
 
 
 //〈interned-strings〉: :=  {‘dir::interned’}〈eol〉( {‘string’}〈eol〉)*
-int parse_interned_strings(list_t*lexems)
+int parse_interned_strings(list_t*lexems,pyobj_t obj)
 {
   if(0==next_lexem_is(lexems, "dir::interned"))
   {
@@ -606,7 +606,7 @@ int parse_interned_strings(list_t*lexems)
   return 0;
 }
 //〈constants〉: :=  {‘dir::consts’}〈eol〉(〈constant〉〈eol〉)*
-int parse_constants(list_t*lexems)
+int parse_constants(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing Constants\n");
   if(0==next_lexem_is(lexems, "dir::consts"))
@@ -633,7 +633,7 @@ int parse_constants(list_t*lexems)
   return 0;
 }
 //〈constant〉: :=  {‘integer’} | {‘float’} | {‘string’} | {‘pycst’} |〈list〉|〈tuple〉
-int parse_constant(list_t*lexems)
+int parse_constant(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing Constant\n");
   if(next_lexem_is(lexems, "integer")||next_lexem_is(lexems,"float"))
@@ -655,7 +655,7 @@ int parse_constant(list_t*lexems)
   return -1;
 }
 //〈list〉: :=  {‘brack::left’}〈constant〉* {‘brack::right’}
-int parse_list(list_t*lexems)
+int parse_list(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing List\n");
   if(0==next_lexem_is(lexems, "brack::left"))
@@ -677,7 +677,7 @@ int parse_list(list_t*lexems)
   return 0;
 }
 //〈tuple〉: :=  {‘paren::left’}〈constant〉* {‘paren::right’}
-int parse_tuple(list_t*lexems)
+int parse_tuple(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing tuple\n");
   if(next_lexem_is(lexems,"left")==0)
@@ -699,7 +699,7 @@ int parse_tuple(list_t*lexems)
   return 0;
 }
 //〈names〉: :=  {‘dir::names’}〈eol〉( {‘string’}〈eol〉)*
-int parse_names(list_t*lexems)
+int parse_names(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing names\n");
   if(next_lexem_is(lexems,"dir::names")==0)
@@ -726,7 +726,7 @@ int parse_names(list_t*lexems)
   return 0;
 }
 //〈varnames〉: :=  {‘dir::varnames’}〈eol〉( {‘string’}〈eol〉)*
-int parse_varnames(list_t*lexems)
+int parse_varnames(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing varnames\n");
   if(next_lexem_is(lexems,"dir::varnames")==0)
@@ -753,7 +753,7 @@ int parse_varnames(list_t*lexems)
   return 0;
 }
 //〈freevars〉: :=  {‘dir::freevars’}〈eol〉( {‘string’}〈eol〉)*
-int parse_freevars(list_t*lexems)
+int parse_freevars(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing freevars\n");
   if(next_lexem_is(lexems,"dir::freevars")==0)
@@ -780,7 +780,7 @@ int parse_freevars(list_t*lexems)
   return 0;
 }
 //〈cellvars〉: :=  {‘dir::cellvars’}〈eol〉( {‘string’}〈eol〉)*
-int parse_cellvars(list_t*lexems)
+int parse_cellvars(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing cellvars\n");
   if(next_lexem_is(lexems,"dir::cellvars")==0)
@@ -807,7 +807,7 @@ int parse_cellvars(list_t*lexems)
   return 0;
 }
 //〈code〉: :=  {‘dir::text’}〈eol〉(〈assembly-line〉〈eol〉)*
-int parse_code(list_t*lexems)
+int parse_code(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing code\n");
   if(next_lexem_is(lexems,"dir::text")==0)
@@ -834,7 +834,7 @@ int parse_code(list_t*lexems)
   return 0;
 }
 //〈assembly-line〉: :=〈insn〉|〈source-lineno〉|〈label〉
-int parse_assembly_line(list_t*lexems)
+int parse_assembly_line(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing assembly line\n");
   if(parse_insn(lexems)==0)
@@ -872,7 +872,7 @@ int parse_label(list_t*lexems)
   return 0;
 }
 //〈source-lineno〉: :=  {‘dir::line’} {‘blank’} {‘integer::dec’}
-int parse_source_lineno(list_t*lexems)
+int parse_source_lineno(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing Lineno\n");
   if(-1==next_lexem_is(lexems,"dir::line"))
@@ -892,7 +892,7 @@ int parse_source_lineno(list_t*lexems)
   return 0;
 }
 //〈insn〉: :=  {‘insn::0’}|    {‘insn::1’} ( {‘integer::dec’} | {‘symbol’} )
-int parse_insn(list_t*lexems)
+int parse_insn(list_t*lexems,pyobj_t obj)
 {
   printf("Parsing insn \n");
   if(-1==next_lexem_is(lexems,"insn::"))
@@ -907,12 +907,15 @@ int parse_insn(list_t*lexems)
   return 0;
 }
 //〈eol〉: :=  ([{‘blank’}] [{‘comment’}] {‘newline’})* [{‘blank’}]
-int parse_eol(list_t*lexems)
+int parse_eol(list_t*lexems, pyobj_t obj)
 {
-  while ( next_lexem_is( lexems, "newline" ) ) {
-      *lexems = (*lexems)->next;
-  }
   printf("Parsing eol\n");
-  while()
-
+  if(-1==next_lexem_is(lexems,"newline"))
+  {
+    return -1;
+  }
+  while ( next_lexem_is( lexems, "newline" ) ) {
+      lexem_advance(lexems);
+  }
+  return 0;
 }
