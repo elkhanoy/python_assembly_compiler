96ca5cb4d6707fdddfefb9a109af7bc26d113f56
phelma <phelma@localhost.localdomain>
2021-11-03 07:31:45 -0400

    modif de list_mnemo

--- include/pyas/list_mnemo_tools.h
+++ include/pyas/list_mnemo_tools.h
@@ -4,9 +4,17 @@ struct contenu_point_text
 {
   int lineno; //Retiens la ligne de la mnémonique
   char*mnemo; //Nom de la mnémonique
-  int nb_param; //Nombre de paramètres pour la mnémonique
+  int param; //paramètre pour la mnémonique: -1 si pas de paramètre
   struct contenu_point_text * next;
-} txt_t, list_txt_t;
+} txt_t, *list_txt_t;
+
+
+
 
 list_txt_t enqueue( list_txt_t l);
+
 list_txt_t list_txt_new( void );
+
+int     list_txt_empty( list_txt_t l );
+
+list_txt_t text_section_list(FILE*file_conf,list_txt_t list_section_text)
