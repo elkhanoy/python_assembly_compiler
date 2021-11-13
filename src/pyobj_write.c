d0baeee5fffbbba989c50e99185e49071ba746d6
phelma <phelma@localhost.localdomain>
2021-11-13 11:12:21 -0500

    pyobj_write modifié avec les hexa

--- src/pyobj_write.c
+++ src/pyobj_write.c
@@ -11,44 +11,63 @@ int pyobj_write( FILE *fp, pyobj_t obj)
   if(NULL==fp) {perror("Erreur ouverture ecriture de bytecode.bin\n"); return -1;}
 
 //////////////////////////////////////////////////////////////////////////
+int k;
 // Header
   //version_pyvm
   char v_pyvm[]={'0','3',' ','f','3'};
   fwrite(v_pyvm,sizeof(char),sizeof(v_pyvm),fp);
 
   //magic number
-  uint32_t lilend_magic;
-  lilend_magic=htole32(((((obj->py).codeblock)->binary).header.magic));
-  fwrite(&lilend_magic,sizeof(lilend_magic),1,fp);
+  char hex_magic[60];
+  sprintf(hex_magic,"%x",((((obj->py).codeblock)->binary).header.magic));
+  for(k=strlen(hex_magic);k<=0;k--)
+  {
+    fwrite(hex_magic,sizeof(char),strlen(hex_magic),fp);
+  }
 
   //timestamp
-  uint32_t lilend_timefile;
-  lilend_timefile=htole32((uint32_t)((((obj->py).codeblock)->binary).header.timestamp));
-  fwrite(&lilend_timefile,sizeof(lilend_timefile),1,fp);
+  char hex_timestamp[60];
+  sprintf(hex_timestamp,"%x",(uint32_t)((((obj->py).codeblock)->binary).header.timestamp));
+  for(k=strlen(hex_timestamp);k<=0;k--)
+  {
+    fwrite(hex_timestamp,sizeof(char),strlen(hex_timestamp),fp);
+  }
 
   //CODE
   char code[]={'6','3'};
   fwrite(code,sizeof(char),1,fp);
 
   //arg_count
-  uint32_t lilend_arg_count;
-  lilend_arg_count=htole32(((((obj->py).codeblock)->header).arg_count));
-  fwrite(&lilend_arg_count,sizeof(lilend_arg_count),1,fp);
+  char hex_arg_count[60];
+  sprintf(hex_arg_count,"%x",((((obj->py).codeblock)->header).arg_count));
+  for(k=strlen(hex_arg_count);k<=0;k--)
+  {
+    fwrite(hex_arg_count,sizeof(char),strlen(hex_arg_count),fp);
+  }
 
   //local_count
-  uint32_t lilend_local_count;
-  lilend_local_count=htole32(((((obj->py).codeblock)->header).local_count));
-  fwrite(&lilend_local_count,sizeof(lilend_local_count),1,fp);
+  char hex_local_count[60];
+  sprintf(hex_local_count,"%x",((((obj->py).codeblock)->header).local_count));
+  for(k=strlen(hex_local_count);k<=0;k--)
+  {
+    fwrite(hex_local_count,sizeof(char),strlen(hex_local_count),fp);
+  }
 
   //stack_size
-  uint32_t lilend_stack_size;
-  lilend_stack_size=htole32(((((obj->py).codeblock)->header).stack_size));
-  fwrite(&lilend_stack_size,sizeof(lilend_stack_size),1,fp);
+  char hex_stack_size[60];
+  sprintf(hex_stack_size,"%x",((((obj->py).codeblock)->header).stack_size));
+  for(k=strlen(hex_stack_size);k<=0;k--)
+  {
+    fwrite(hex_stack_size,sizeof(char),strlen(hex_stack_size),fp);
+  }
 
   //flags
-  uint32_t lilend_flags;
-  lilend_flags=htole32(((((obj->py).codeblock)->header).flags));
-  fwrite(&lilend_flags,sizeof(lilend_flags),1,fp);
+  char hex_flags[60];
+  sprintf(hex_flags,"%x",((((obj->py).codeblock)->header).flags));
+  for(k=strlen(hex_flags);k<=0;k--)
+  {
+    fwrite(hex_flags,sizeof(char),strlen(hex_flags),fp);
+  }
 
   //////////////////////////////////////////////////////////////////////////
  /// Body
@@ -61,26 +80,29 @@ int pyobj_write( FILE *fp, pyobj_t obj)
     //marqueur de tuple
   fwrite("28",sizeof("28"),1,fp);
     //nombre de consts
-  uint32_t lilend_consts;
-  lilend_consts=htole32((((((obj->py).codeblock)->binary).content.consts)->py).list.size);
-  fwrite(&lilend_consts,sizeof(lilend_consts),1,fp);
+    char hex_consts_size[60];
+    sprintf(hex_consts_size,"%x",(((((obj->py).codeblock)->binary).content.consts)->py).list.size);
+    for(k=strlen(hex_consts_size);k<=0;k--)
+    {
+      fwrite(hex_consts_size,sizeof(char),strlen(hex_consts_size),fp);
+    }
 
     //valeurs de consts
   int i,j;
-  uint32_t consts_len;
-  uint32_t lilend_consts_len;
-
 
   for(i=0;i<((((((obj->py).codeblock)->binary).content.consts)->py).list.size);i++)
   {
     // Le type d'une des valeurs TO DO une fonction sur les types...
 
     // La taille d'une des valeurs
-    consts_len=(((((((((obj->py).codeblock)->binary).content.consts)->py).list.value)[i])->py).string.length);
-    lilend_consts_len=htole32(consts_len);
-    fwrite(&lilend_consts_len,sizeof(lilend_consts_len),1,fp);
+    char hex_consts_length[60];
+    sprintf(hex_consts_length,"%x",(((((((((obj->py).codeblock)->binary).content.consts)->py).list.value)[i])->py).string.length));
+    for(k=strlen(hex_consts_length);k<=0;k--)
+    {
+      fwrite(hex_consts_length,sizeof(char),strlen(hex_consts_length),fp);
+    }
     // La valeur
-    for(j=consts_len;j<=0;j--)
+    for(j=(((((((((obj->py).codeblock)->binary).content.consts)->py).list.value)[i])->py).string.length);j<=0;j--)
     {
       (((((((obj->py).codeblock)->binary).content.consts)->py.list.value)[i])->py.string.buffer)[j]=(int)(((((((obj->py).codeblock)->binary).content.consts)->py.list.value)[i])->py.string.buffer)[j];
       fwrite(&(((((((obj->py).codeblock)->binary).content.consts)->py.list.value)[i])->py.string.buffer)[j],sizeof((((((((obj->py).codeblock)->binary).content.consts)->py.list.value)[i])->py.string.buffer)[j]),1,fp);
@@ -90,21 +112,25 @@ int pyobj_write( FILE *fp, pyobj_t obj)
   //tuple noms de symboles
   fwrite("28",sizeof("28"),1,fp);
 
-  uint32_t lilend_names;
-  lilend_names=htole32((((((obj->py).codeblock)->binary).content.names)->py).list.size);
-  fwrite(&lilend_names,sizeof(lilend_names),1,fp);
+  char hex_names_size[60];
+  sprintf(hex_names_size,"%x",(((((obj->py).codeblock)->binary).content.names)->py).list.size);
+  for(k=strlen(hex_names_size);k<=0;k--)
+  {
+    fwrite(hex_names_size,sizeof(char),strlen(hex_names_size),fp);
+  }
 
-  uint32_t names_len;
-  uint32_t lilend_names_len;
   for(i=0;i<((((((obj->py).codeblock)->binary).content.names)->py).list.size);i++)
   {
     // type
     // taille
-    names_len=((((((((obj->py).codeblock)->binary).content.names)->py).list.value)[i])->py).string.length;
-    lilend_names_len=htole32(names_len);
-    fwrite(&lilend_names_len,sizeof(lilend_names_len),1,fp);
+    char hex_names_length[60];
+    sprintf(hex_names_length,"%x",(((((((((obj->py).codeblock)->binary).content.names)->py).list.value)[i])->py).string.length));
+    for(k=strlen(hex_names_length);k<=0;k--)
+    {
+      fwrite(hex_names_length,sizeof(char),strlen(hex_names_length),fp);
+    }
     //valeur
-    for(j=names_len;j<=0;j--)
+    for(j=(((((((((obj->py).codeblock)->binary).content.names)->py).list.value)[i])->py).string.length);j<=0;j--)
     {
       ((((((((obj->py).codeblock)->binary.content.names)->py).list.value)[i])->py).string.buffer)[j]=(int)(((((((obj->py).codeblock)->binary).content.names)->py.list.value)[i])->py.string.buffer)[j];
       fwrite(&(((((((obj->py).codeblock)->binary).content.names)->py.list.value)[i])->py.string.buffer)[j],sizeof((((((((obj->py).codeblock)->binary).content.names)->py.list.value)[i])->py.string.buffer)[j]),1,fp);
@@ -114,22 +140,26 @@ int pyobj_write( FILE *fp, pyobj_t obj)
   //tuple noms de variables
   fwrite("28",sizeof("28"),1,fp);
 
-  uint32_t lilend_varnames;
-  lilend_consts=htole32((((((obj->py).codeblock)->binary).content.varnames)->py).list.size);
-  fwrite(&lilend_varnames,sizeof(lilend_varnames),1,fp);
-
-  uint32_t varnames_len;
-  uint32_t lilend_varnames_len;
+  char hex_varnames_size[60];
+  sprintf(hex_varnames_size,"%x",(((((obj->py).codeblock)->binary).content.varnames)->py).list.size);
+  for(k=strlen(hex_varnames_size);k<=0;k--)
+  {
+    fwrite(hex_varnames_size,sizeof(char),strlen(hex_varnames_size),fp);
+  }
 
   for(i=0;i<((((((obj->py).codeblock)->binary).content.varnames)->py).list.size);i++)
   {
     // type
     //size
-    varnames_len=((((((((obj->py).codeblock)->binary).content.varnames)->py).list.value)[i])->py).string.length;
-    lilend_varnames_len=htole32(varnames_len);
-    fwrite(&lilend_varnames_len,sizeof(lilend_varnames_len),1,fp);
+    char hex_varnames_length[60];
+    sprintf(hex_varnames_length,"%x",(((((((((obj->py).codeblock)->binary).content.varnames)->py).list.value)[i])->py).string.length));
+    for(k=strlen(hex_varnames_length);k<=0;k--)
+    {
+      fwrite(hex_varnames_length,sizeof(char),strlen(hex_varnames_length),fp);
+    }
+
     //valeur
-    for(j=varnames_len;j<=0;j--)
+    for(j=((((((((obj->py).codeblock)->binary).content.varnames)->py).list.value)[i])->py).string.length;j<=0;j--)
     {
       ((((((((obj->py).codeblock)->binary.content.varnames)->py).list.value)[i])->py).string.buffer)[j]=(int)(((((((obj->py).codeblock)->binary).content.varnames)->py.list.value)[i])->py.string.buffer)[j];
       fwrite(&(((((((obj->py).codeblock)->binary).content.varnames)->py.list.value)[i])->py.string.buffer)[j],sizeof((((((((obj->py).codeblock)->binary).content.varnames)->py.list.value)[i])->py.string.buffer)[j]),1,fp);
@@ -139,20 +169,23 @@ int pyobj_write( FILE *fp, pyobj_t obj)
 
   //tuple noms de variables libres
   fwrite("28",sizeof("28"),1,fp);
-//type à faire
 
-  uint32_t lilend_freevars;
-  lilend_freevars=htole32(((((obj->py).codeblock->binary).content.freevars)->py).list.size);
-  fwrite(&lilend_freevars,sizeof(lilend_freevars),1,fp);
+  char hex_freevars_size[60];
+  sprintf(hex_freevars_size,"%x",(((((obj->py).codeblock)->binary).content.freevars)->py).list.size);
+  for(k=strlen(hex_freevars_size);k<=0;k--)
+  {
+    fwrite(hex_freevars_size,sizeof(char),strlen(hex_freevars_size),fp);
+  }
 
-  uint32_t freevars_len;
-  uint32_t lilend_freevars_len;
 
   for(i=0;i<((((((obj->py).codeblock)->binary).content.freevars)->py).list.size);i++)
   {
-    freevars_len=((((((((obj->py).codeblock)->binary).content.freevars)->py).list.value)[i])->py).string.length;
-    lilend_freevars_len=htole32(freevars_len);
-    fwrite(&lilend_freevars_len,sizeof(lilend_freevars_len),1,fp);
+    char hex_freevars_length[60];
+    sprintf(hex_freevars_length,"%x",(((((((((obj->py).codeblock)->binary).content.freevars)->py).list.value)[i])->py).string.length));
+    for(k=strlen(hex_freevars_length);k<=0;k--)
+    {
+      fwrite(hex_freevars_length,sizeof(char),strlen(hex_freevars_length),fp);
+    }
 
     for(j=((((((obj->py).codeblock)->binary.content.freevars)->py.list.value)[i])->py.string.length);j<=0;j--)
     {
@@ -163,19 +196,23 @@ int pyobj_write( FILE *fp, pyobj_t obj)
 
   //tuple cellules
   fwrite("28",sizeof("28"),1,fp);
-  //type à faire
-  uint32_t lilend_cellvars;
-  lilend_cellvars=htole32(((((obj->py).codeblock)->binary).content.cellvars)->py.list.size);
-  fwrite(&lilend_cellvars,sizeof(lilend_cellvars),1,fp);
 
-  uint32_t cellvars_len;
-  uint32_t lilend_cellvars_len;
+  char hex_cellvars_size[60];
+  sprintf(hex_cellvars_size,"%x",(((((obj->py).codeblock)->binary).content.cellvars)->py).list.size);
+  for(k=strlen(hex_cellvars_size);k<=0;k--)
+  {
+    fwrite(hex_cellvars_size,sizeof(char),strlen(hex_cellvars_size),fp);
+  }
+
 
   for(i=0;i<((((((obj->py).codeblock)->binary).content.cellvars)->py).list.size);i++)
   {
-    cellvars_len=(((((((obj->py).codeblock)->binary).content.cellvars)->py).list.value)[i])->py.string.length;
-    lilend_cellvars_len=htole32(cellvars_len);
-    fwrite(&lilend_cellvars_len,sizeof(lilend_cellvars_len),1,fp);
+    char hex_cellvars_length[60];
+    sprintf(hex_cellvars_length,"%x",(((((((((obj->py).codeblock)->binary).content.cellvars)->py).list.value)[i])->py).string.length));
+    for(k=strlen(hex_cellvars_length);k<=0;k--)
+    {
+      fwrite(hex_cellvars_length,sizeof(char),strlen(hex_cellvars_length),fp);
+    }
 
     for(j=((((((obj->py).codeblock)->binary.content.cellvars)->py.list.value)[i])->py.string.length);j<=0;j--)
     {
@@ -190,11 +227,12 @@ int pyobj_write( FILE *fp, pyobj_t obj)
   //nom du fichier source python filename
   fwrite("73",sizeof("73"),1,fp); //marqueur de string
 
-  uint32_t filename_len;
-  uint32_t lilend_filename_len;
-  filename_len=(uint32_t)(((((obj->py).codeblock)->binary).trailer.filename)->py).string.length;
-  lilend_filename_len=htole32(filename_len);
-  fwrite(&lilend_filename_len,sizeof(lilend_filename_len),1,fp);
+  char hex_filename_length[60];
+  sprintf(hex_filename_length,"%x",(((((obj->py).codeblock)->binary).trailer.filename)->py).string.length);
+  for(k=strlen(hex_filename_length);k<=0;k--)
+  {
+    fwrite(hex_filename_length,sizeof(char),strlen(hex_filename_length),fp);
+  }
 
   for(i=(((((obj->py).codeblock)->binary).trailer.filename)->py).string.length;i<=0;i++)
   {
@@ -205,11 +243,12 @@ int pyobj_write( FILE *fp, pyobj_t obj)
   //nom du code name
   fwrite("73",sizeof("73"),1,fp);
 
-  uint32_t name_len;
-  uint32_t lilend_name_len;
-  name_len=(uint32_t)(((((obj->py).codeblock)->binary).trailer.name)->py).string.length;
-  lilend_name_len=htole32(name_len);
-  fwrite(&lilend_name_len,sizeof(lilend_name_len),1,fp);
+  char hex_name_length[60];
+  sprintf(hex_name_length,"%x",(((((obj->py).codeblock)->binary).trailer.name)->py).string.length);
+  for(k=strlen(hex_name_length);k<=0;k--)
+  {
+    fwrite(hex_name_length,sizeof(char),strlen(hex_name_length),fp);
+  }
 
   for(i=(((((obj->py).codeblock)->binary).trailer.name)->py).string.length;i<=0;i++)
   {
@@ -218,11 +257,12 @@ int pyobj_write( FILE *fp, pyobj_t obj)
   }
 
   //entier donnant le num de la première ligne de code
-  uint32_t firstlineno;
-  uint32_t lilend_firstlineno;
-  firstlineno=((((obj->py).codeblock)->binary).trailer.firstlineno);
-  lilend_firstlineno=htole32(firstlineno);
-  fwrite(&lilend_firstlineno,sizeof(lilend_firstlineno),1,fp);
+  char hex_lineno[60];
+  sprintf(hex_lineno,"%x",((((obj->py).codeblock)->binary).trailer.firstlineno));
+  for(k=strlen(hex_lineno);k<=0;k--)
+  {
+    fwrite(hex_lineno,sizeof(char),strlen(hex_lineno),fp);
+  }
 
   //lnotab
   fwrite("73",sizeof("73"),1,fp);
